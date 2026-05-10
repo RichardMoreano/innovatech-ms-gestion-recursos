6c69730d3ac11e643c8f3fa7d745c5f0548980ac
Ghost <s21243004@gmail.com>
2026-05-10 19:28:46 -0400

    feat: configuración inicial Resource Service - puerto 8082

---
+++ src/main/java/cl/duoc/innovatech/innovatech_microservicios_base/InnovatechMicroserviciosBaseApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.innovatech.innovatech_microservicios_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovatechMicroserviciosBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovatechMicroserviciosBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/innovatech/innovatech_microservicios_base/InnovatechMicroserviciosBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.innovatech.innovatech_microservicios_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovatechMicroserviciosBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
